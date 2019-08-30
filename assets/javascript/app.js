111a9b16e0d31b7def880a47a3ded7644f5846bf
Peter Wu <23109224+PapaWuski@users.noreply.github.com>
2019-08-30 10:32:50 -0700

    new day in class

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,55 @@
-console.log("hellowrodl")
+// jQuery('div')
+// $('div')
+// var pTag$ = $("<p>")
+// var div$ = $("div.root")
+
+// // $(pTag$).text("textString");
+
+// // div$.append(pTag$)
+// // $(selector).text(textString);
+// for(var i=0; i<100; i++){
+// pTag$ = $("<p>" )
+// $(pTag$).text(`stextString ${i}`);
+// // $(pTag$).attr("style", "color:blue").attr("src-img","small")
+// $(pTag$).attr({
+//     class:"item",
+//     "sorce-img":"small",
+// });
+// $(pTag$).addClass("item big");
+// $(pTag$).css("color", "red");
+// pTag$.css({
+//     backgroundColor:"orange",
+//     padding:"50px"
+// })
+// $(div$).append(pTag$);
+// }
+
+// var div$ = $("div.root");
+
+// var btn = $("<button>");
+// btn.text("Submit1");
+// $(btn).val("first");
+// $(div$).append(btn);
+// var btn = $("<button>");
+// btn.text("Submit2");
+// $(btn).val("second");
+// $(div$).append(btn);
+// var btn = $("<button>");
+// btn.text("Submit3");
+// $(div$).append(btn);
+// var btn = $("<button>");
+// btn.text("Submit4");
+// $(div$).append(btn);
+
+// $("button").on("click", function() {
+//   $(this).css({
+//     backgroundColor: "red"
+//   });
+//   var value = $(this).val();
+//   if (value ==="second"){
+//       $("div.modal").toggle();
+//   }
+//   console.log(value);
+// });
+
+console.log("helloworld")
